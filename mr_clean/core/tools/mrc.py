fb8aead49a13b88a8527718b1d353226fde12a85
Albert Liu <albertymliu@gmail.com>
2018-07-22 19:57:18 -0400

    done with version 0.0.5

--- mr_clean/core/tools/mrc.py
+++ mr_clean/core/tools/mrc.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
-import pandas as pd
+#import pandas as pd
 import mr_clean.core.functions.basics as basics
 from mr_clean.core.functions.scrub import smart_scrub
-import mr_clean.core.stats.summary as stats
+#import mr_clean.core.stats.summary as stats
 
 def clean(df,error_rate = 0):
     """ Superficially cleans data, i.e. changing simple things about formatting.
@@ -18,35 +18,36 @@ def clean(df,error_rate = 0):
     
     # Change colnames
     basics.clean_colnames(df)
+    # Eventually use a more advanced function to clean colnames
     print('Changed colnames to {}'.format(df.columns))
     
     # Remove extra whitespace
-    for col_name in df.select_dtypes(include = 'object').columns:
+    obj_col_list = df.select_dtypes(include = 'object').columns
+    for col_name in obj_col_list:
         df[col_name] = basics.col_strip(df,col_name)
         print("Stripped extra whitespace from '{}'".format(col_name))
-        smart_coerce(df,col_name)
-        new_dtype = smart_coerce(df,col_name)
+    
+    # Coerce columns if possible
+    for col_name in obj_col_list:
+        new_dtype = coerce_col(df,col_name,error_rate)
         if new_dtype is not None:
             print("Coerced '{}' to datatype '{}'".format(col_name, new_dtype))
 
     # Scrub columns
-    for col_name in df.select_dtypes(include = 'object').columns:
+    obj_col_list = df.select_dtypes(include = 'object').columns
+    for col_name in obj_col_list:
         scrubf, scrubb = smart_scrub(df,col_name,1-error_rate)
         if scrubf is not None or scrubb is not None:
             print("Scrubbed '{}' from the front and '{}' from the back of column '{}'" \
                   .format(scrubf,scrubb,col_name))
-        else:
-            continue
-        new_dtype = smart_coerce(df,col_name)
+    
+    # Coerice columns if possible
+    for col_name in obj_col_list:
+        new_dtype = coerce_col(df,col_name,error_rate)
         if new_dtype is not None:
-            print("Coerced '{}' to datatype '{}'".format(col_name, new_dtype))
-    
-    # Change format of entries to numeric if possible
-    dtype_stats = stats.dtypes_summary(df)
-    ['rows_numerical','rows_string','rows_date_time','category_count','largest_category','rows_na','rows_total']
-    
-    # use dtype stats to infer columns to change
-    
-    
+            print("Coerced '{}' to datatype '{}'".format(col_name, new_dtype))  
     return df
     # TODO For future implementation
+
+def coerce_col(df,col_name,error_rate):
+    pass
